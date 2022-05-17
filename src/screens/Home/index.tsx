f7b4b85d766e7e27eed9e337445bf06d3eaab2c3
Jaqueline Gonzaga <jaqueline_gonzaga@hotmail.com>
2022-05-16 22:17:37 -0400

    feat: flatlist pizzas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Header, Greeting, GreetingEmoji, GreetingText, MenuHeader, MenuItemsNumber, Title } from './styles';
 import happyEmoji from '../../../src/assets/happy.png';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components/native';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Alert, FlatList } from 'react-native';
 import { Search } from '../../components/Search';
+import { ProductCard, ProductProps } from '../../components/ProductCard';
+import firestore from '@react-native-firebase/firestore';
+
 
 export function Home() {
+    const [pizzas, setPizzas] = useState<ProductProps[]>([]);
+    const [products, setProducts] = useState<ProductProps[]>([]);
+    const [search, setSearch] = useState('');
+
     const { COLORS } = useTheme();
+
+    function fetchPizzas(value: string) {
+        const formattedValue = value.toLowerCase().trim();
+        firestore()
+            .collection('pizzas')
+            .orderBy('name_insensitive')
+            .startAt(formattedValue)
+            .endAt(`${formattedValue}\uf8ff`)
+            .get()
+            .then(response => {
+                const data = response.docs.map(doc => {
+                    return {
+                        id: doc.id,
+                        ...doc.data()
+                    }
+                }) as ProductProps[];
+
+                setPizzas(data);
+            })
+            .catch(() => Alert.alert('Erro ao buscar pizzas'));
+    }
+
+    useEffect(() => {
+        fetchPizzas('');
+    }, []);
 
     return (
         <Container>
@@ -25,7 +57,17 @@ export function Home() {
                 <Title>Cardápio</Title>
                 <MenuItemsNumber>10 pizzas</MenuItemsNumber>
             </MenuHeader>
-
+            <FlatList
+                data={pizzas}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <ProductCard data={item} />}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingTop: 20,
+                    paddingBottom: 125,
+                    marginHorizontal: 24
+                }}
+            />
         </Container>
     );
 }
